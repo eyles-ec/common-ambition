9f839efe7dd75775e526d0a4b3714635bde051ec
Emily Eyles <emily.eyles@bristol.ac.uk>
2025-09-29 15:26:08 +0100

    add fcn for codelist flags, add dates
    Added a function to generate flag variables when you have a codelist (or single code).
    
    Generated flag variables for project outcomes
    
    generated year month and week variables per episode for later collapse into counts.

--- data_cleaning/data_clean.R
+++ data_cleaning/data_clean.R
@@ -2,6 +2,16 @@ library(dplyr)
 library(tidyverse)
 library(data.table)
 
+#function for creating codelist flags easily. you can name the new column anything
+
+flag_cl <- function(df, code_list, new_col_name = "has_any_code") {
+  new_col <- sym(new_col_name) #language wrangling
+  df %>%
+    mutate(
+      !!new_col := rowSums(across(starts_with("DiagCode"), ~ . %in% code_list)) > 0 #checks across all rows of DiagCode etc to search for the codelist.
+    )
+}
+
 #set working directory
 
 setwd("Own path")
@@ -36,9 +46,57 @@ unity_cab1_hiv <- unity_cab1_hiv %>%
     )
   )
 
-#save dataset 
+#generate HIV test variables
 
-write.csv(unity_hiv.csv)
+#variable if test declined or inappropriate (Shappt P1B, P1C)
+cl_hiv_dec = c("P1B", "P1C")
 
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, cl_hiv_dec, "declined_hiv_test")
+
+#variable if test conducted (Shappt P1A, T4, T7, T-HIV)
+cl_hiv_test = c("P1A", "T4", "T7", "T-HIV")
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, cl_hiv_test, "hiv_test")
+
+#generate HIV diagnosis variables
+
+#extant HIV (Shappt H, H1X H1AX, H1BX (last three are previoulsy diagnosis elsewhere diagnostic codes))
+cl_extant = c("H", "H1X", "H1AX", "H1BX")
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, cl_extant, "extant_hiv")
+
+#New diagnosis (Shappt H1, H1A, H1B)
+cl_new = c("H1", "H1A", "H1B")
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, cl_new, "new_hiv")
+
+#New diagnosis late (Shappt H1B)
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, "H1B", "new_late_hiv")
+
+#generate PrEP variables
+
+#Starting or continuing PrEP even through other source, including prescriptions (Shappt O41, O42, O43, O51, O52, O53)
+cl_curr_prep <- c("O41", "O42", "O43", "O51", "O52", "O53")
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, cl_curr_prep, "current_prep")
+
+#Declined PrEP (Shappt O44)
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, "O44", "declined_prep")
+
+#Stopped PrEP (Shappt O45)
+
+unity_cab1_hiv <- flag_cl(unity_cab1_hiv, "O45", "stopped_prep")
+
+#generate year, month, and (ISO) week variables
+unity_cab1_hiv$EventDate <- as.Date(unity_cab1_hiv$EventDate, format = "%d/%m/%Y")
+
+unity_cab1_hiv <- unity_cab1_hiv %>%
+  mutate(
+    year = year(EventDate),
+    month = month(EventDate),
+    week = isoweek(EventDate)  # ISO week number (1–53)
+  )
 
   
